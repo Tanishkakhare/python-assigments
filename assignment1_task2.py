c4751ec12406d8cf052597eeca0345cae3e2c465
Tanishka khare <kharetanishka@gmail.com>
2025-07-22 15:03:14 +0530

    Add files via upload
    Tried to create both the program in such a ways that represent the output in the similar ways as needed and showed.

---
+++ assignment1_task2.py
@@ -0,0 +1,6 @@
+# forming a customized sentence
+a = input("Enter your first name:")
+b = input("Enter your last name:")
+c = a+" "+b+"!"
+
+print("hello,",c," Very nice to meet you. \nhow are you?")
