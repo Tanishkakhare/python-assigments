c0c7ec8689fedb59d3e75ea40ccdd69ed235a1d3
Tanishka khare <kharetanishka@gmail.com>
2025-07-23 13:27:58 +0530

    Add files via upload

---
+++ assignment2_task1.py
@@ -0,0 +1,7 @@
+ # program to detect even and odd numbers
+
+number = int(input("enter your number :"))
+if (number % 2 == 0):
+    print(number, "is an even number. ")
+else:
+    print(number, "is an odd number. ")
