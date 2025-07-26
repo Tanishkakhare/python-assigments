5c50772e413d246d46154e01c5cac7e7e6828599
Tanishka khare <kharetanishka@gmail.com>
2025-07-26 15:22:45 +0530

    Add files via upload

---
+++ assignment5_task2.py
@@ -0,0 +1,10 @@
+numbers = list(range(1, 11))
+print("original list:",numbers)
+first_five = numbers[:5]
+
+
+reversed_five = first_five[::-1]
+
+
+print("Extracted first five elements:", first_five)
+print("Reversed extracted elements:", reversed_five)
